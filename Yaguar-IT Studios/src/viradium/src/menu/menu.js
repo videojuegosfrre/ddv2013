9b5b2ea21423b25940c7653628665f36f44432c4
Sebastián J. Seba <ssebastianj@gmail.com>
2013-11-02 01:34:02 -0300

    Actualizo Cocos2D HTML5 a versión del repositorio. Pruebas con botones de mouse y teclado.
    Signed-off-by: Sebastián J. Seba <ssebastianj@gmail.com>

--- Yaguar-IT Studios/src/viradium/src/menu/menu.js
+++ Yaguar-IT Studios/src/viradium/src/menu/menu.js
@@ -1,3 +1,6 @@
+var lastEvent;
+var heldKeys = {};
+
 var JetSprite = cc.Sprite.extend({
   _currentRotation:0,
 
@@ -15,13 +18,18 @@ var JetSprite = cc.Sprite.extend({
     if(e === cc.KEY.left)
     {
       this._currentRotation--;
-
     }
-    else if(e === cc.KEY.right)
+    else if(e === cc.KEY.right) {
       this._currentRotation++;
+    }
 
-    if(this._currentRotation < 0) this._currentRotation = 360;
-    if(this._currentRotation > 360) this._currentRotation = 0;
+    if(this._currentRotation < 0) {
+        this._currentRotation = 360;
+    }
+
+    if(this._currentRotation > 360) {
+        this._currentRotation = 0;
+    }
   },
 
   handleTouch:function(touchLocation)
@@ -44,13 +52,14 @@ var JetSprite = cc.Sprite.extend({
 });
 
 var MenuLayer = cc.LayerColor.extend({
+    _debug:cc.COCOS2D_DEBUG,
+
     init:function()
     {
         this._super(new cc.Color4B(0, 0, 0, 255));
 
         var size = cc.Director.getInstance().getWinSize();
-
-        audio_engine = cc.AudioEngine.getInstance();
+        var audio_engine = cc.AudioEngine.getInstance();
 
         audio_engine.setEffectsVolume(0.5);
         audio_engine.setMusicVolume(0.5);
@@ -70,18 +79,54 @@ var MenuLayer = cc.LayerColor.extend({
 
         this.addChild(menu);
 
+        // Check for mouse support
+        if ('mouse' in sys.capabilities) {
+            cc.log("Mouse Supported. Enabling...");
+            this.setMouseEnabled(true);
+        }
+        else {
+            cc.log("Mouse Not Supported");
+        }
+
+        // Check for keyboard support
+        if ('keyboard' in sys.capabilities) {
+            cc.log("Keyboard Supported. Enabling...");
+            this.setKeyboardEnabled(true);
+        }
+        else {
+            cc.log("Keyboard Not Supported");
+        }
+
+        //Check for touch support
+        if ('touches' in sys.capabilities) {
+            cc.log("Touch Supported. Enabling...");
+            this.setTouchEnabled(true);
+        }
+        else {
+            cc.log("Touch Not Supported");
+        }
+
+        // Schedule update
+        cc.log("Scheduling update...");
+        this.scheduleUpdate();
+
         return this;
     },
+
     playSound:function(){
         cc.log("Playing sound");
-        audio_engine.playEffect(s_effect);
+        cc.AudioEngine.getInstance().playEffect(s_effect);
     },
+
     playSong:function(){
         cc.log("Playing song");
-        audio_engine.playMusic(s_background_music);
+        cc.AudioEngine.getInstance().playMusic(s_background_music);
     },
+
     stopPlayingSound:function(){
         cc.log("Done playing song");
+        var audio_engine = cc.AudioEngine.getInstance();
+
         if(audio_engine.isMusicPlaying())
         {
             cc.log("Stoping song");
@@ -91,8 +136,85 @@ var MenuLayer = cc.LayerColor.extend({
             cc.log("Music is not playing");
         }
     },
-    exit:function(){
+
+    exit:function () {
       cc.log("Exit");
+    },
+
+    onMouseDown:function (event) {
+        this._showMouseButtonInfo(event, "Down");
+    },
+
+    onMouseUp:function (event) {
+        this._showMouseButtonInfo(event, "Up");
+    },
+
+    onMouseMoved:function (event) {
+    },
+
+    onMouseDragged:function (event) {
+        this._showMouseButtonInfo(event, "Dragged");
+    },
+
+    onRightMouseDown:function (event) {
+        this._showMouseButtonInfo(event, "Down");
+    },
+
+    onRightMouseUp:function (event) {
+        this._showMouseButtonInfo(event, "Up");
+    },
+
+    onRightMouseMoved:function (event) {
+    },
+
+    onRightMouseDragged:function (event) {
+        this._showMouseButtonInfo(event, "Dragged");
+    },
+
+    onScrollWheel:function (event) {
+        cc.log("Scroll Wheel - Delta: " + event.getWheelDelta());
+    },
+
+    onKeyDown:function (e) {
+        if (lastEvent && lastEvent == e) {
+            return;
+        }
+
+        lastEvent = e;
+        heldKeys[e] = true;
+    },
+
+    onKeyUp:function (e) {
+        lastEvent = null;
+        delete heldKeys[e];
+    },
+
+    update: function (dt) {
+        cc.log(heldKeys);
+    },
+
+    _showMouseButtonInfo:function (event, trigger) {
+        if (this._debug !== 0) {
+            var button = null;
+
+            switch (event._button) {
+                case 0:
+                    button = 'Izquierdo';
+                    break;
+                case 1:
+                    button = 'Central';
+                    break;
+                case 2:
+                    button = 'Derecho';
+                    break;
+                default:
+                    button = 'Desconocido';
+            }
+
+            cc.log("Mouse " + trigger + " - Button: " + button + " - Pos: (" +
+                   event.getLocation().x + ", " +
+                   event.getLocation().y + ")");
+        }
     }
 });
 
